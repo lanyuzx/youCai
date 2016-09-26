ba9c224ba11e7e85bdd260442e8564ab2ff1ddfb
lanyuzx <lanyuzx@qq.com>
2016-09-26 18:44:17 +0800

    周尊贤
    修改一丢丢

--- 高仿有菜/Classes/首页/Controller/LLHomeDetailViewController.swift
+++ 高仿有菜/Classes/首页/Controller/LLHomeDetailViewController.swift
@@ -107,6 +107,10 @@ extension LLHomeDetailViewController:UITableViewDataSource,UITableViewDelegate {
         return cell!
     
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
   
     
 }
